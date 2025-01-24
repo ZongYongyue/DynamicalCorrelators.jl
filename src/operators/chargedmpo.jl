ea4ce5f92bf9aed7f76b4fd47b45589b446859c7
ZongYongyue <zongyyphy@qq.com>
2025-01-24 10:36:02 +0800

    add new method for chargedmpo

--- src/operators/chargedmpo.jl
+++ src/operators/chargedmpo.jl
@@ -19,3 +19,15 @@ function chargedMPO(operator::AbstractTensorMap, site::Integer, nsites::Integer)
     end
     return mpo
 end
+
+function chargedMPO(operator₁::AbstractTensorMap, operator₂::AbstractTensorMap, site₁::Integer, site₂::Integer, nsites::Integer)
+    pspace = domain(operator₁)[1]
+    if (length(domain(operator₁)) == 2)&&(length(codomain(operator₁)) == 1)&&(length(codomain(operator₂)) == 2)&&(length(domain(operator₂)) == 1)&&(site₁ < site₂)
+        Z, vspace = fZ(operator₁), domain(operator₁)[2]
+        I = isomorphism(storagetype(operator₁), oneunit(vspace)*pspace, pspace*oneunit(vspace))
+        mpo = FiniteMPO([i < site₁ ? I : i == site₁ ? add_single_util_leg(operator₁) : (site₁ < i < site₂) ? Z : i == site₂ ? add_single_util_leg(operator₂) : I for i in 1:nsites])
+    else
+        throw(ArgumentError("invalid operator, expected operator₁ at left and operator₂ at right"))
+    end
+    return mpo
+end
