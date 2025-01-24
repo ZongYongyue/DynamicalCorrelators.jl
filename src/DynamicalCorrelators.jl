49847b39d6e3077a5ceea9dbb9571e13dd9c24d3
ZongYongyue <zongyyphy@qq.com>
2025-01-24 10:33:53 +0800

    fit MPSKit@v0.12.1 and MPSKitModels@v0.4.0

--- src/DynamicalCorrelators.jl
+++ src/DynamicalCorrelators.jl
@@ -1,32 +1,38 @@
 module DynamicalCorrelators
 
 using LinearAlgebra: norm
-using QuantumLattices: Hilbert, Term, Lattice, Neighbors, bonds, OperatorGenerator, Operator, CompositeIndex #CoordinatedIndex, FockIndex, Index, OperatorSet
+using QuantumLattices: Hilbert, Term, Lattice, Neighbors, bonds, OperatorGenerator, Operator, CompositeIndex, CoordinatedIndex, FockIndex, Index, OperatorSet
 using TensorKit: FermionParity, U1Irrep, SU2Irrep, Vect, Sector, ProductSector, AbstractTensorMap, TensorMap
 using TensorKit: truncdim, truncerr, truncspace, truncbelow, ←, space, numout, numin, dual, fuse
-using TensorKit: ⊠, ⊗, permute, domain, codomain, isomorphism, storagetype, @planar, @tensor, blocks, flip
-using MPSKit: FiniteMPS, FiniteMPO, MPOHamiltonian, TDVP, TDVP2
-using MPSKit: add_util_leg, timestep, environments
-using MPSKitModels: contract_onesite, contract_twosite, @mpoham, _firstspace, _lastspace, vertices, nearest_neighbours, next_nearest_neighbours
+using TensorKit: ⊠, ⊗, permute, domain, codomain, isomorphism, storagetype, @planar, @tensor, blocks, block, flip
+using MPSKit: FiniteMPS, FiniteMPO, FiniteMPOHamiltonian, MPOHamiltonian, TDVP, TDVP2
+using MPSKit: add_util_leg, _firstspace, _lastspace, timestep, environments
+using KrylovKit: exponentiate
+using MPSKitModels: contract_onesite, contract_twosite, @mpoham, vertices, nearest_neighbours, next_nearest_neighbours
 using MPSKitModels: InfiniteChain, InfiniteCylinder, InfiniteHelix, InfiniteLadder, FiniteChain, FiniteCylinder, FiniteStrip, FiniteHelix, FiniteLadder
 using Distributed: @sync, @distributed, workers, addprocs
 using SharedArrays: SharedArray
+using JLD2: save, load
+using Printf: @printf, @sprintf
 
 import QuantumLattices: expand
 import MPSKit: propagator, dot
 
+#export matsubara
+
 export hubbard
 
-export fZ, e_plus, e_min, number, onsiteCoulomb, S_plus, S_min, S_z, S_square, b_plus, b_min, j_l
+export fZ, e_plus, e_min, number, onsiteCoulomb, S_plus, S_min, S_z, S_square, b_plus, b_min#, j_l
 export chargedMPO, hamiltonian
 
-export add_single_util_leg
+export add_single_util_leg, execute, execute!
 
 export chargedMPS, randFiniteMPS
 
 export propagator, dcorrelator
 export RetardedGF, GreaterLessGF, MatsubaraGF
 
+include("benchmark/matsubara.jl")
 include("models/hamiltonians.jl")
 include("models/lattices.jl")
 include("operators/fermions.jl")
