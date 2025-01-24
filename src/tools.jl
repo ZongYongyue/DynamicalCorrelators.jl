667a2cbe87df441d6d26e0d8ead108a4180523db
ZongYongyue <zongyyphy@qq.com>
2025-01-24 10:34:34 +0800

    add execute and execute!

--- src/tools.jl
+++ src/tools.jl
@@ -11,3 +11,34 @@ function add_single_util_leg(tensor::AbstractTensorMap{S,N1,N2}) where {S,N1,N2}
     end
     return fourlegtensor
 end
+
+function execute(f::Function, args; name::String="name", info::String="info", id::String="id", cachepath::String="./", kwargs...)
+    cache_file_path = joinpath(cachepath, @sprintf "%s_%s_%s.jld2" name id info)
+    if isfile(cache_file_path)
+        task = load(cache_file_path, "task")
+        println("Load from $cache_file_path")
+    else
+        if !isdir(cachepath)
+        mkdir(cachepath)
+        println("Cache directory created at $cachepath")
+        end
+        task = f(args...; kwargs...)
+        save(cache_file_path, "task", task, "info", info, "id", id)
+        println("Save as $cache_file_path")
+    end
+    return task
+end
+
+function execute!(f::Function, args; name::String="name", info::String="info", id::String="id", cachepath::String="./", kwargs...)
+    cache_file_path = joinpath(cachepath, @sprintf "%s_%s_%s.jld2" name id info)
+    if isfile(cache_file_path)
+        Println("Task has finished or file with same name has existed!")
+    else
+        if !isdir(cachepath)
+            mkdir(cachepath)
+            println("Cache directory created at $cachepath")
+        end
+        save(cache_file_path, "task", f(args...; kwargs...), "info", info, "id", id)
+        println("Save as $cache_file_path")
+    end
+end
