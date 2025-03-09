cc377e55e9f56c6c3bb68aeb84d6a0ba4034f166
ZongYongyue <zongyyphy@qq.com>
2025-03-09 11:38:25 +0800

    add BilayerSquare lattice

--- src/models/lattices.jl
+++ src/models/lattices.jl
@@ -1,7 +1,14 @@
 #pre-defined lattices
 
 struct BilayerSquare
-    N::Int
-    U::Int
-    L::Int
+    W::Integer
+    L::Integer
+    norbit::Integer
+    lattice::Lattice
+end
+
+function BilayerSquare(W::Integer, L::Integer; norbit::Integer=2, periodic::Bool=true)
+    unitcell = Lattice([0, 0, 0], [0, 0, 1]; vectors = [[1, 0, 0], [0, 1, 0]], name=:BS)
+    lattice = periodic ? Lattice(unitcell, (W, L), ('p', 'o')) : Lattice(unitcell, (W, L), ('o', 'o'))
+    return BilayerSquare(W, L, norbit, lattice)
 end
