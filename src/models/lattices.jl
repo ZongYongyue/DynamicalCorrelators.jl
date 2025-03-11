25cd580c81e7f6a050b5e51aed2586ea3fcdaeda
ZongYongyue <zongyyphy@qq.com>
2025-03-11 20:14:26 +0800

    add CustomLattice

--- src/models/lattices.jl
+++ src/models/lattices.jl
@@ -1,14 +1,59 @@
 #pre-defined lattices
 
-struct BilayerSquare
-    W::Integer
-    L::Integer
-    norbit::Integer
-    lattice::Lattice
+abstract type CustomLattice end
+
+struct BilayerSquare{T<:Integer, U<:AbstractLattice, L<:AbstractLattice} <: CustomLattice
+    W::T
+    L::T
+    unitcell::U
+    lattice::L
+    indices::AbstractArray{<:AbstractArray{T, 1}, 1}
 end
 
-function BilayerSquare(W::Integer, L::Integer; norbit::Integer=2, periodic::Bool=true)
-    unitcell = Lattice([0, 0, 0], [0, 0, 1]; vectors = [[1, 0, 0], [0, 1, 0]], name=:BS)
+function BilayerSquare(W::Integer, L::Integer; 
+                    norbit::Integer=2, 
+                    periodic::Bool=false)
+    unitcell = Lattice([0,0,0],[0,0,1]; vectors=[[1,0,0],[0,1,0]], name=:BS)
     lattice = periodic ? Lattice(unitcell, (W, L), ('p', 'o')) : Lattice(unitcell, (W, L), ('o', 'o'))
-    return BilayerSquare(W, L, norbit, lattice)
+    indeces = [[(i-1)*norbit + j for j in 1:norbit] for i in 1:length(lattice)]
+    return BilayerSquare(W, L, unitcell, lattice, indeces)
+end
+
+function twosite_bonds(customlattice::CustomLattice, a::Integer, b::Integer;
+    intralayer::Bool=true,
+    neighbors::Neighbors=Neighbors(1=>1))
+    bs = intralayer ? filter(bond->bond.points[1].rcoordinate[3] == bond.points[2].rcoordinate[3], bonds(customlattice.lattice, neighbors)) : filter(bond->bond.points[1].rcoordinate[3] !== bond.points[2].rcoordinate[3], bonds(customlattice.lattice, neighbors))
+    tbs = Vector(undef, length(bs))
+    for (o, bond) in enumerate(bs)
+    (i, j) = bond.points[1].site > bond.points[2].site ? (bond.points[2].site, bond.points[1].site) : (bond.points[1].site, bond.points[2].site)   
+    tbs[o] = (customlattice.indices[i][a], customlattice.indices[j][b])
+    end
+    return tbs
+end
+
+function onesite_bonds(customlattice::CustomLattice, a::Integer, b::Integer)
+    bs =  bonds(customlattice.lattice, 0)
+    obs = Vector(undef, length(bs))
+    for i in eachindex(bs) 
+    obs[i] = (customlattice.indices[i][a], customlattice.indices[i][b])
+    end
+    return obs
+end
+
+function onesite_bonds(customlattice::CustomLattice, a::Integer)
+    bs =  bonds(customlattice.lattice, 0)
+    obs = Vector(undef, length(bs))
+    for i in eachindex(bs) 
+    obs[i] = (customlattice.indices[i][a],)
+    end
+    return obs
+end
+
+function find_position(indices::AbstractArray{<:AbstractArray{<:Integer, 1}, 1}, target::Integer)
+    for (i, inner) in enumerate(indices)
+        if target in inner
+            return i
+        end
+    end
+    return nothing 
 end
