9bc97c387002d0af7161ce9ecf9e40f63d78a996
ZongYongyue <zongyyphy@qq.com>
2025-05-17 18:03:14 +0800

    add Correlation,PairingCorrelation,SpinCorrelation,correlator

--- src/observables/correlator.jl
+++ src/observables/correlator.jl
@@ -1,8 +1,42 @@
 """
 
 """
-# abstract type Pairing end
+abstract type Correlation end
 
-# function correlator(O₁::AbstractTensorMap)
-#     O₁
-# end
+struct PairingCorrelation{K, L<:CustomLattice} <: Correlation
+    operator::AbstractTensorMap
+    lattice:: L
+    amplitude::Union{Nothing, Function}
+end
+
+
+struct SpinCorrelation{K} <: Correlation
+    operator::AbstractTensorMap
+    amplitude::Union{Nothing, Function}
+end
+
+function correlator(O::AbstractTensorMap, bra::AbstractFiniteMPS, gs::AbstractFiniteMPS, index::Integer)
+    dot(bra, chargedMPS(O, gs, index))
+end
+
+function correlator(O::AbstractTensorMap, gs::AbstractFiniteMPS, index::Integer, ket::AbstractFiniteMPS)
+    dot(chargedMPS(O, gs, index), ket)
+end
+
+
+function correlator(O::AbstractTensorMap, bra::AbstractFiniteMPS, gs::AbstractFiniteMPS, index::NTuple{2, Integer})
+    dot(bra, chargedMPS(O, gs, index))
+end
+
+function correlator(O::AbstractTensorMap, gs::AbstractFiniteMPS, index::NTuple{2, Integer}, ket::AbstractFiniteMPS)
+    dot(chargedMPS(O, gs, index), ket)
+end
+
+
+function correlator(O::AbstractTensorMap, gs::AbstractFiniteMPS, indices::NTuple{2, Integer})
+    dot(chargedMPS(O, gs, indices[1]), chargedMPS(O, gs, indices[2]))
+end
+
+function correlator(O::AbstractTensorMap, gs::AbstractFiniteMPS, indices::NTuple{4, Integer})
+    dot(chargedMPS(O, gs, indices[1], indices[2]), chargedMPS(O, gs, indices[3], indices[4]))
+end
