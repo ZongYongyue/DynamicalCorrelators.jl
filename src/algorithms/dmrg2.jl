1b2d1ef7befe48006cea617f1e0b81bd9fa8a1fd
ZongYongyue <zongyyphy@qq.com>
2025-06-25 21:03:01 +0800

    add dmrg2_sweep!

--- src/algorithms/dmrg2.jl
+++ src/algorithms/dmrg2.jl
@@ -72,3 +72,67 @@ end
 function dmrg2(ψ, H, truncdims; kwargs...)
     return dmrg2!(copy(ψ), H, truncdims; kwargs...)
 end
+
+function dmrg2_sweep!(iter::Integer, ψ::AbstractFiniteMPS, H, trscheme::TruncationScheme, ϵs::AbstractArray; alg::DMRG2=DefaultDMRG, filename::String="default_dmrg2.jld2", verbose::Union{Bool, Integer}=true)
+    start_time = now()
+    ϵ = maximum(ϵs)
+    if iter == 1
+        Int(verbose) > 0 && println("[Sweep Started]: ", Dates.format(start_time, "d.u yyyy HH:MM"))
+        Int(verbose) > 0 && flush(stdout)
+    end
+    alg_eigsolve = updatetol(alg.alg_eigsolve, iter, ϵ)
+    zerovector!(ϵs)
+    errs = zeros(Float64, length(ψ))
+    # left to right sweep
+    envs = environments(ψ, H)
+    for pos in 1:(length(ψ) - 1)
+        @plansor ac2[-1 -2; -3 -4] := ψ.AC[pos][-1 -2; 1] * ψ.AR[pos + 1][1 -4; -3]
+        Hac2 = AC2_hamiltonian(pos, ψ, H, ψ, envs)
+        _, vecs, _ = eigsolve(Hac2, ac2, 1, :SR, alg_eigsolve)
+        al, c, ar, err = tsvd!(vecs[1]; p=2, trunc=trscheme, alg=alg.alg_svd)
+        normalize!(c)
+        v = @plansor ac2[1 2; 3 4] * conj(al[1 2; 5]) * conj(c[5; 6]) * conj(ar[6; 3 4])
+        ϵs[pos] = abs(1 - abs(v))
+        errs[pos] = err^2
+        ψ.AC[pos] = (al, complex(c))
+        ψ.AC[pos + 1] = (complex(c), _transpose_front(ar))
+        Int(verbose) > 1 && println("  SweepL2R: site $(pos) => site $(pos+1) ", Dates.format(now(), "d.u yyyy HH:MM"))
+        Int(verbose) > 1 && flush(stdout)
+    end
+
+    # right to left sweep
+    for pos in (length(ψ) - 2):-1:1
+        @plansor ac2[-1 -2; -3 -4] := ψ.AL[pos][-1 -2; 1] * ψ.AC[pos + 1][1 -4; -3]
+        Hac2 = AC2_hamiltonian(pos, ψ, H, ψ, envs)
+        _, vecs, _ = eigsolve(Hac2, ac2, 1, :SR, alg_eigsolve)
+        al, c, ar, err = tsvd!(vecs[1]; p=2, trunc=trscheme, alg=alg.alg_svd)
+        normalize!(c)
+        v = @plansor ac2[1 2; 3 4] * conj(al[1 2; 5]) * conj(c[5; 6]) * conj(ar[6; 3 4])
+        ϵs[pos] = max(ϵs[pos], abs(1 - abs(v)))
+        errs[pos] = max(errs[pos], err^2)
+        ψ.AC[pos + 1] = (complex(c), _transpose_front(ar))
+        ψ.AC[pos] = (al, complex(c))
+        Int(verbose) > 1 && println("  SweepR2L: site $(pos) <= site $(pos+1) ", Dates.format(now(), "d.u yyyy HH:MM"))
+        Int(verbose) > 1 && flush(stdout)
+    end
+
+    err = maximum(errs)
+    ϵ = maximum(ϵs)
+    D = length(ψ) <= 4 ? dim(domain(ψ[length(ψ)÷2])) : maximum([dim(domain(ψ[length(ψ)÷2])), dim(domain(ψ[length(ψ)÷2+1])), dim(domain(ψ[length(ψ)÷2-1]))])
+    E₀ = expectation_value(ψ, H, envs)
+    current_time = now()
+    Int(verbose) > 0 && println("[Sweep $(iter)]", " | duration:", Dates.canonicalize(current_time-start_time))
+    println("  E₀ = $(E₀), D = $(D), err² = $(err), ϵ = $(ϵ)")
+    flush(stdout)
+    mode = (iter == 1 ? "w" : "a")
+    jldopen(filename, mode) do f
+        f["sweep_$(iter)_ψ"] = ψ
+        f["sweep_$(iter)_ϵ"]  = ϵ
+        f["sweep_$(iter)_ϵs"]  = ϵs
+        f["sweep_$(iter)_err"] = err
+        f["sweep_$(iter)_errs"] = errs
+        f["sweep_$(iter)_D"]  = D
+    end
+    GC.gc()
+    return nothing
+end
