e9dcb8eee0decf26252e807892d947cf48886d3b
ZongYongyue <zongyyphy@qq.com>
2025-08-11 19:01:39 +0800

    include spin.jl

--- src/DynamicalCorrelators.jl
+++ src/DynamicalCorrelators.jl
@@ -32,6 +32,7 @@ include("models/hamiltonians.jl")
 export hubbard, hubbard_bilayer_2band, kitaev_hubbard
 
 include("operators/fermions.jl")
+include("operators/spin.jl")
 export fZ, e_plus, e_min, hopping, σz_hopping, number, onsiteCoulomb, S_plus, S_min, S_z, S_square, neiborCoulomb, heisenberg, spinflip, pairhopping
 export singlet_dagger, singlet, triplet_dagger, triplet
 
