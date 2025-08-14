a63b7f2bcf10761bf4ca4b509f040b7a670d8404
ZongYongyue <zongyyphy@qq.com>
2025-08-14 17:09:31 +0800

    add new method for A_expiHt_B

--- src/observables/dcorrelator.jl
+++ src/observables/dcorrelator.jl
@@ -22,11 +22,11 @@ function expiHt(H::MPOHamiltonian, ts::AbstractVector, rho::FiniteMPO=identityMP
     flush(stdout)
     envs = environments(rho_mps, H)
     jldopen(filename, "w") do f
-        f["ts"] = ts
+        f["t=$(ts[i])"] = rho
     end
     for i in 2:length(ts)
         alg = i > n ? DefaultTDVP : DefaultTDVP2(trscheme)
-        rho_mps, envs = timestep!(rho_mps, H, 0, ts[i]-ts[i-1], alg, envs)
+        rho_mps, envs = timestep(rho_mps, H, 0, ts[i]-ts[i-1], alg, envs)
         current_time = now()
         verbose && println("[$i/$(length(ts))] t = $(ts[i]) ", " | duration:", Dates.canonicalize(current_time-start_time))
         flush(stdout)
@@ -37,8 +37,7 @@ function expiHt(H::MPOHamiltonian, ts::AbstractVector, rho::FiniteMPO=identityMP
         end
         start_time = current_time
     end
-    record_end = now()
-    verbose && println("Ended: ", Dates.format(record_end, "d.u yyyy HH:MM"), " | total duration: ", Dates.canonicalize(record_end-record_start))
+    verbose && println("Ended: ", Dates.format(record_end, "d.u yyyy HH:MM"), " | total duration: ", Dates.canonicalize(now()-record_start))
 
     return convert(FiniteMPO, rho_mps)
 end
@@ -51,6 +50,7 @@ function A_expiHt_B(::Type{R}, gs::AbstractFiniteMPS, H::MPOHamiltonian, ops::Tu
                     n::Integer=3, 
                     trscheme=truncerr(1e-3)) where R<:RetardedGF
     gsenergy = expectation_value(gs, H)
+    mps = [[chargedMPS(ops[1], gs, j) for j in 1:length(gs)]; [chargedMPS(ops[2], gs, j) for j in 1:length(gs)]]
     gf = zeros(ComplexF64, (2*length(H)), length(H), length(ts))
     rho_mps = convert(FiniteMPS, rho)
     evolve_start, evolve_finish, record_start = now(), now(), now()
@@ -72,23 +72,23 @@ function A_expiHt_B(::Type{R}, gs::AbstractFiniteMPS, H::MPOHamiltonian, ops::Tu
 
         idx = Threads.Atomic{Int}(1)
         indices = CartesianIndices((2*length(H), length(H)))
+        m = length(indices)
         Threads.@sync for _ in 1:parallel
             Threads.@spawn while true
                 j = Threads.atomic_add!(idx, 1) 
-                j > length(indices) && break  
-                b, a = indices[j].I
-                if b <= length(H)
-                    gf[b,a,i] = dot(chargedMPS(ops[1], gs, a), rho, chargedMPS(ops[1], gs, b))
+                j > m && break  
+                a, b = indices[j].I
+                if a <= length(H)
+                    gf[a,b,i] = dot(mps[b], rho, mps[a])
                 else
-                    gf[b,a,i] = conj(dot(chargedMPS(ops[2], gs, a), rho, chargedMPS(ops[2], gs, b-length(H))))
+                    gf[a,b,i] = conj(dot(mps[b], rho, mps[a-length(H)]))
                 end
             end
         end
-
         factor₁, factor₂ = -im*exp(im*gsenergy*ts[i]), -im*exp(-im*gsenergy*ts[i])
         gf[1:length(H),:,i] = factor₁*gf[1:length(H),:,i]
         gf[(length(H)+1):end,:,i] = factor₂*gf[(length(H)+1):end,:,i]
-
+        GC.gc()
         jldopen(filename, "a") do f
             f["gf_$(ts[i])"] = gf[:,:,i]
             if save_all || i == length(ts)
@@ -105,6 +105,74 @@ function A_expiHt_B(::Type{R}, gs::AbstractFiniteMPS, H::MPOHamiltonian, ops::Tu
         f["gfs"] = gfs
     end
     return gfs
+end
+
+function A_expiHt_B(::Type{GreaterLessGF}, gs::AbstractFiniteMPS, H::MPOHamiltonian, ops::Tuple{<:AbstractTensorMap, <:AbstractTensorMap}, ts::AbstractVector, rho::FiniteMPO=identityMPO(H);
+                    verbose=true, 
+                    which=:greater,
+                    filename::String="default_gfs.jld2", 
+                    save_all=false,  
+                    parallel::Union{String, Integer}=Threads.nthreads(), 
+                    n::Integer=3, 
+                    trscheme=truncerr(1e-3))
+    gsenergy = expectation_value(gs, H) 
+    gf = zeros(ComplexF64, (length(H)), length(H), length(ts))
+    rho_mps = convert(FiniteMPS, rho)
+    mps = which==:greater ? [chargedMPS(ops[1], gs, j) for j in 1:length(gs)] : [chargedMPS(ops[2], gs, j) for j in 1:length(gs)]
+    evolve_start, evolve_finish, record_start = now(), now(), now()
+    verbose && println("[1/$(length(ts))] t = $(ts[1]) ", " | Started:", Dates.format(record_start, "d.u yyyy HH:MM"))
+    flush(stdout)
+    envs = environments(rho_mps, H)
+    jldopen(filename, "w") do f
+        f["ts"] = ts
+    end
+    for i in eachindex(ts)
+        if i > 1
+            alg = i > n ? DefaultTDVP : DefaultTDVP2(trscheme)
+            rho_mps, envs = timestep(rho_mps, H, 0, ts[i]-ts[i-1], alg, envs)
+            rho = convert(FiniteMPO, rho_mps)
+            evolve_finish = now()
+            verbose && println("[$i/$(length(ts))] t = $(ts[i]) ", " | duration:", Dates.canonicalize(evolve_finish-evolve_start))
+            flush(stdout)
+        end
+
+        if parallel == 1
+            for j in axes(gf, 1)
+                for k in axes(gf, 2)
+                    gf[j,k,i] = which==:greater ? -im*exp(im*gsenergy*ts[i])*dot(mps[k], rho, mps[j]) : -im*exp(-im*gsenergy*ts[i])*conj(dot(mps[k], rho, mps[j]))
+                end
+            end
+            GC.gc()
+        else
+            idx = Threads.Atomic{Int}(1)
+            indices = CartesianIndices((length(H), length(H)))
+            m = length(indices)
+            Threads.@sync for _ in 1:parallel
+                Threads.@spawn while true
+                    j = Threads.atomic_add!(idx, 1) 
+                    j > m && break  
+                    a, b = indices[j].I
+                    gf[a,b,i] = which==:greater ? -im*exp(im*gsenergy*ts[i])*dot(mps[b], rho, mps[a]) : -im*exp(-im*gsenergy*ts[i])*conj(dot(mps[b], rho, mps[a]))
+                end
+            end
+            GC.gc()
+        end
+
+        jldopen(filename, "a") do f
+            f["gf_$(ts[i])"] = gf[:,:,i]
+            if save_all || i == length(ts)
+                f["rho_$(ts[i])"] = rho
+            end
+        end
+        verbose && println("    gf_$(ts[i]) is done ", " | duration:", Dates.canonicalize(now() - evolve_finish))
+        evolve_start = now()
+    end
+
+    verbose && println("Ended: ", Dates.format(now(), "d.u yyyy HH:MM"), " | total duration: ", Dates.canonicalize(now()-record_start))
+    jldopen(filename, "a") do f
+        f["gfs"] = gf
+    end
+    return gf
 end
 
 """
