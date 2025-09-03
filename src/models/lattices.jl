626b3a80a65824e474e3cde137c78f23084fe470
ZongYongyue <zongyyphy@qq.com>
2025-09-03 15:50:07 +0800

    add Custom, snake_2D, kitaev_bonds

--- src/models/lattices.jl
+++ src/models/lattices.jl
@@ -79,3 +79,49 @@ function Square(W::Integer, L::Integer;
     indeces = [[(i-1)*norbit + j for j in 1:norbit] for i in 1:length(lattice)]
     return Square(W, L, unitcell, lattice, indeces)
 end
+
+
+struct Custom{T<:Integer, L<:QLattice} <: CustomLattice
+    lattice::L
+    indices::AbstractArray{<:AbstractArray{T, 1}, 1}
+end
+
+function Custom(lattice::QLattice; norbit=1)
+    indeces = [[(i-1)*norbit + j for j in 1:norbit] for i in 1:length(lattice)]
+    return Custom(lattice, indeces)
+end
+
+
+function snake_2D(directions, orders)
+    lattice = Vector{Vector}(undef, length(orders)+1)
+    lattice[1] = [0, 0]
+    for i in eachindex(orders) 
+        if orders[i] > 0
+            lattice[i+1] = lattice[i] + directions[orders[i]]
+        else
+            lattice[i+1] = lattice[i] - directions[abs(orders[i])]
+        end
+    end
+    return lattice
+end
+
+function kitaev_bonds(directions, lattice)
+    bs = bonds(lattice, Neighbors(1=>1))
+    indices₁, indices₂, indices₃ = [], [], []
+    for bond in bs
+        bd = bond.points[1].rcoordinate - bond.points[2].rcoordinate
+        if abs(dot(bd,directions[1])) ≈ norm(bd)*norm(directions[1])
+            (a, b) = bond.points[1].site > bond.points[2].site ? (bond.points[2].site, bond.points[1].site) : (bond.points[1].site, bond.points[2].site)
+            push!(indices₁, (a, b))
+        elseif abs(dot(bd,directions[2])) ≈ norm(bd)*norm(directions[2])
+            (a, b) = bond.points[1].site > bond.points[2].site ? (bond.points[2].site, bond.points[1].site) : (bond.points[1].site, bond.points[2].site)
+            push!(indices₂, (a, b))
+        elseif abs(dot(bd,directions[3])) ≈ norm(bd)*norm(directions[3])
+            (a, b) = bond.points[1].site > bond.points[2].site ? (bond.points[2].site, bond.points[1].site) : (bond.points[1].site, bond.points[2].site)
+            push!(indices₃, (a, b))
+        else
+            throw(ArgumentError("bond is dismatched with any directions"))
+        end
+    end
+    return indices₁, indices₂, indices₃
+end
