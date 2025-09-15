4f416917a9a8112d6497d72b13bdc8676472e169
ZongYongyue <zongyyphy@qq.com>
2025-09-15 14:40:52 +0800

    fix conductivity

--- src/observables/conductivity.jl
+++ src/observables/conductivity.jl
@@ -1,12 +1,12 @@
 #conductivity based on single-particle Green's function: A. Georges, G. Kotliar, W. Krauth, and M. J. Rozenberg, Dynamical Mean-Field Theory of Strongly Correlated Fermion Systems and the Limit of Infinite Dimensions, Rev. Mod. Phys. 68, 13 (1996).
 
-function conductivity(∂Ek::AbstractArray, gf::AbstractArray, ω_range::AbstractArray)
-    f(ω) = 1/(exp(β*ω)+1)
+function conductivity(β::Number, ∂Ek::AbstractArray, gf::AbstractArray, ω_range::AbstractArray; μ=0)
+    f(ω) = 1/(exp(β*(ω-μ))+1)
     ∂f(ω) = -β*f(ω)*(1-f(ω))
     σ = 0
     for k in eachindex(∂Ek)
         for (i, ω) in enumerate(ω_range)
-            σ += ((∂Ek[k])^2)*((gf[k][i])^2)*(-∂f(ω))
+            σ += ((∂Ek[k])^2)*(((-1/π)*(tr(gf[i][k]).im))^2)*(-∂f(ω))
         end
     end
     return σ
